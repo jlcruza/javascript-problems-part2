08cc7aa7e2098bc54b397b13069ea6296f693e07
jlcruza <jlcruz7125@gmail.com>
2019-07-13 16:06:47 -0400

    Second Exercise Done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,12 +1,9 @@
 const palindromes = function(a) {
-    let str1 = a.split("");
-    let str2 = a.split("").reverse();
+    let str1 = a.toLowerCase().split("");
+    let str2 = a.toLowerCase().split("").reverse();
 
-    str1.filter(isAlphaOrParen);
-    str2.filter(isAlphaOrParen);
-
-    console.log(str1);
-    console.log(str2);
+    str1 = cleanString(str1);
+    str2 = cleanString(str2);
 
     for(let i =0; i<str1.length;i++){
         if(str1[i] != str2[i]){
@@ -14,6 +11,16 @@ const palindromes = function(a) {
         }
     }
     return true;
+}
+
+function cleanString(msg){
+    let res = "";
+    for(let i = 0; i<msg.length;i++){
+        if(isAlphaOrParen(msg[i])){
+            res += msg[i];
+        }
+    }
+    return res;
 }
 
 function isAlphaOrParen(str) {
