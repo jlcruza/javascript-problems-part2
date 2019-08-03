fcf44502916b2cdf968f5a748723725232cb4712
Jorge Cruz <jlcruz7125@gmail.com>
2019-08-02 22:14:38 -0400

    fibonacci test complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if(!isNaN(value) && value>0){
+        let a = b = 1;
+        if(value==1 || value == 2){
+            return 1;
+        }
+        for(let i=3; i<=Number(value);i++){
+            let c = a + b;
+            a= b;
+            b = c;
+        }
+        return b;
+
+    }
+    else{
+        return "OOPS";
+    }
 
 }
 
