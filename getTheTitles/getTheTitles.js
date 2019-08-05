9e3381febfd8e0a82fad0696fdb261b979677735
Jorge Cruz <jlcruz7125@gmail.com>
2019-08-05 18:57:28 -0400

    getTheTitles Completed

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    books.forEach(element => {
+        if(element.hasOwnProperty("title")){
+            titles.push(element.title);
+        }
+    });
+    return titles;
 }
 
 module.exports = getTheTitles;
