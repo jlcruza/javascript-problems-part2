feaf6c695264c2e1309cc6ca1c36e2da75790b8e
Jorge Cruz <jlcruz7125@gmail.com>
2019-08-05 18:50:47 -0400

    findTheOldest Done

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    let age = 0;
+    let highest = 0;
+    let oldest = people[0];
+    people.forEach(element => {
+        if(element.hasOwnProperty("yearOfDeath")){
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
+        else{
+            let d = new Date();
+            age =  d.getFullYear() - element.yearOfBirth;
+        }
+        if(age > highest){
+            highest = age;
+            oldest = element;
+        }
+    });
+    
+    return oldest;
 }
 
 module.exports = findTheOldest
